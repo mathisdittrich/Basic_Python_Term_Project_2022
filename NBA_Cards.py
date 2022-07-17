9657f2f1ba98385144f1accf6e4c009fbb96de0e
Johannes Dittrich <mathisdittrich@gmail.com>
2022-07-17 12:55:56 +0200

    Card + Values

--- NBA_Cards.py
+++ NBA_Cards.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.image as mpimg
+from PIL import Image, ImageFont, ImageDraw 
+
 
 
 # read the data
@@ -10,10 +13,11 @@ Generell_Information = pd.read_csv('./Data/Player_Generell_Information.csv')
 Stats = pd.read_csv('./Data/Player_Stats.csv')
 
 ################ Data preprocessing - Start ############################################################
-# Generell Information
-Generell_Information = Generell_Information[["Name", "Position","Age", "Height"]]
 
-# Stats 
+# Generell Information
+Generell_Information = Generell_Information[["Name", "Position","Age", "Height", "Weight"]]
+
+# only Stats from the season 2021/22
 Stats = Stats[Stats["Season"] == "2021-22"]
 
 # add Rebound Column
@@ -29,6 +33,42 @@ Stats = Stats.rename({"Player": "Name"}, axis= "columns")
 # merge 
 joint_Data = pd.merge(Generell_Information, Stats, on="Name", how = "right")
 
-################ Data preprocessing - End ############################################################
+################ Card Visualisation ############################################################
 
-print(joint_Data)
+def showCard(Name):
+
+    Player = joint_Data[joint_Data["Name"] == Name]
+
+    my_image = Image.open("card.jpeg")
+
+    image_editable = ImageDraw.Draw(my_image)
+
+    for (index, values) in pd.DataFrame.iterrows(Player):
+        image_editable.text((200,140), values["Name"], (0, 0, 0))
+        image_editable.text((200,160), values["Tm"], (0, 0, 0))
+        image_editable.text((200,180), values["Position"], (0, 0, 0))
+        image_editable.text((200,200), str(values["Height"]), (0, 0, 0))
+        image_editable.text((200,220), str(values["Weight"]), (0, 0, 0))
+        image_editable.text((200,240), str(values["AST"]), (0, 0, 0))
+       
+        print(values)
+
+    # display generell information
+    # title_font = ImageFont.truetype('playfair/playfair-font.ttf', 200)
+   
+
+    my_image.save("result.jpg")
+
+    plt.imshow(mpimg.imread('result.jpg'))
+    plt.show()
+
+
+
+Input = "Trae Young"
+
+
+showCard(Input)
+
+
+
+
